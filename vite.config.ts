971616efda8b3eb6670aa2009ad48f606117328f
Armored Pawn <69987903+Armoredpawn@users.noreply.github.com>
2025-07-26 12:35:40 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ScienceGlimpse/",
   server: {
-    base: "/ScienceGlimpse/",
     host: "::",
     port: 8080,
   },
