df6a89935cae473b61b18d286fbcf5157ea4249f
Armoredpawn <ayush25srivastava25@gmail.com>
2025-10-25 11:08:36 -0400

    Removed search bar

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -102,7 +102,7 @@ const Articles = () => {
             </p>
 
             {/* Search */}
-            <div className="relative max-w-md">
+            {/*<div className="relative max-w-md">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-4 h-4" />
               <Input
                 placeholder="Search articles..."
@@ -110,7 +110,7 @@ const Articles = () => {
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="pl-10"
               />
-            </div>
+            </div>*/}
           </div>
 
           {/* Articles Grid */}
